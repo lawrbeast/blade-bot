924cd5a077ac72a99d46dc2f2a2420f4e3c2e533
qLau <38216868+qLau@users.noreply.github.com>
2018-04-09 17:42:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -50,9 +50,8 @@ bot.on("message", async message => {
     //   if(err) console.log(err)
     // });
 
-    let prefix = prefixes[message.guild.id].prefixes;
 
-    //let prefix = botconfig.prefix;
+    let prefix = botconfig.prefix;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
