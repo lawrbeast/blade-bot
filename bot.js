5b1e2857824b2c465bdb024a70e93abef86746dc
qLau <38216868+qLau@users.noreply.github.com>
2018-05-04 22:30:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,7 +115,7 @@ if (msg.includes(`DISCORD.GG`)){
 	// VERIFICATION
 	if(msg.includes(`ACCEPT`){
 	  	let role = message.guild.roles.find(`name`, `Member`);
-		message.member.addRole(role);
+		member.addRole(role);
 	return
 	   }
   });
