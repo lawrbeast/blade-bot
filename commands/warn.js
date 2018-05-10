3462636734e14f7eac27f866dad230ef9303f3d4
qLau <38216868+qLau@users.noreply.github.com>
2018-05-10 17:22:29 +0300

    Create warn.js

---
+++ commands/warn.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+        let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+        let warnuEmbed = new Discord.RichEmbed()
+        .setTitle ("~**Comanda Warn**~")
+        .setColor("#f90000")
+        .addField("Te rog sa folosesti 竊ｴ", "!warn @User Motiv")
+        .setFooter("~Comanda Warn~");
+
+        let cfu = new Discord.RichEmbed()
+        .setTitle ("Eroare!")
+        .setColor("#f90000")
+        .addField("User negasit", "Nu gasesc acel user!");
+        if(!wUser) return message.channel.send(cfu).then(msg => {msg.delete(5000)});
+        let wReason = args.join(" ").slice(22);
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nu ai permisiunile necesare.");
+        if(wUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nu poti face asta unui administrator.");
+
+        message.channel.send(`**${wUser} a fost avertizat!**`)});
+        wUser.send(`Ai primit warn pe **${message.guild.name}** de la **${message.author.tag}** cu motivul **${wReason}**`)
+}
+
+module.exports.help = {
+    name: "warn"
+}
