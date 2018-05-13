0f33ee1b1785ebfb67c5d7dbef8a292c9500c2b2
qLau <38216868+qLau@users.noreply.github.com>
2018-05-13 18:57:40 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,13 +3,14 @@ const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
  //!clear 15
  let mspEmbed = new Discord.RichEmbed()
- .setFooter(`${message.guild.name}`, `${message.guild.iconURL}`)
- if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry_sign: Nu aveți permisiunea pentru a face asta!");
+ .setDescription("Nu poti face asta deoarece nu ai permisiunile necesare.")
+ .setColor("#bc2731")
+ if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send({embed:mspEmbed});
  let clearEmbed = new Discord.RichEmbed()
  .setFooter(`${message.guild.name}`, `${message.guild.iconURL}`)
  .setColor("#bc2731")
  .addField("Folosește ", "w!clear număr mesaje");
- if(!args[0]) return message.channel.send(clearEmbed);
+ if(!args[0]) return message.channel.send({embed:clearEmbed});
  message.channel.bulkDelete(args[0]).then(() => {
      message.channel.send.then(msg => msg.delete(10000));
  });
