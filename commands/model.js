805889c52969f48da1d8b90fb956dda11206eeac
qLau <38216868+qLau@users.noreply.github.com>
2018-09-11 21:12:10 +0300

    Update model.js

--- commands/model.js
+++ commands/model.js
@@ -3,7 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
   let dailyembed = new Discord.RichEmbed()
   .setTitle("Daily Battle")
+  .setColor('#f71d54')
   .setDescription(":black_small_square: Temă:\n:black_small_square: Stock:\n:black_small_square: Tipul lucrării:\n:black_small_square: Dimensiuni:\n:black_small_square: Text:");
+  if(!author.id === '257491128671141888') return message.delete();
   message.channel.send({embed:dailyembed});
 }
 module.exports.help = {
