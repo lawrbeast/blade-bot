0e633f41a193dece10fdb8adab8d79b7f0482c2e
qLau <38216868+qLau@users.noreply.github.com>
2018-09-11 21:19:44 +0300

    Update model.js

--- commands/model.js
+++ commands/model.js
@@ -5,7 +5,7 @@ module.exports.run = (bot, message, args) => {
   .setTitle("Daily Battle")
   .setColor('#f71d54')
   .setDescription(":black_small_square: Temă:\n:black_small_square: Stock:\n:black_small_square: Tipul lucrării:\n:black_small_square: Dimensiuni:\n:black_small_square: Text:");
-  if[args[0] === "daily") {
+  if(args[0] === "daily") {
  message.channel.send({embed:dailyembed})
 }
  if[args[0] === "battle") {
