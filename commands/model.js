d6c6f76541da4237c2d4ed54cb48d7712e0d032c
qLau <38216868+qLau@users.noreply.github.com>
2018-09-11 20:58:31 +0300

    Update model.js

--- commands/model.js
+++ commands/model.js
@@ -3,9 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
   let dailyembed = new Discord.RichEmbed()
   .setTitle("Model Daily Battle")
-  .setDescription("test");
+  .setDescription(":black_small_square: Temă:\n:black_small_square: Stock:\n:black_small_square: Tipul lucrării:\n:black_small_square: Dimensiuni:\n:black_small_square: Text:");
 let args1 = args.join(" ").slice(1);
-  if(args1) return message.channel.send({embed:dailyembed}); 
+  if(args1) ='daily' return message.channel.send({embed:dailyembed});
+  if(!args1) return message.channel.send.("Te rog sa introduci numele modelului!")
 }
 module.exports.help = {
     name: "model"
