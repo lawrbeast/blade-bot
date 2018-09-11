cfea5e6778c8bc051043c1de35b25fe66c82d0f9
qLau <38216868+qLau@users.noreply.github.com>
2018-09-11 20:52:50 +0300

    Update model.js

--- commands/model.js
+++ commands/model.js
@@ -5,7 +5,7 @@ module.exports.run = (bot, message, args) => {
   .setTitle("Model Daily Battle")
   .setDescription("test");
 let args1 = args.join(" ").slice(1);
-  if(args1) = "daily" {
+  if(args1) === "daily" {
     message.channel.send({embed:dailyembed});
   }
   
