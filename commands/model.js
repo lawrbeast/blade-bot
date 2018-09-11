cc4f1128a9e0f919f0e322c3cce152a32d7b0e2e
qLau <38216868+qLau@users.noreply.github.com>
2018-09-11 20:45:50 +0300

    Update model.js

--- commands/model.js
+++ commands/model.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = (bot, message, args) => {
- let args = args.join(" ").slice(1)
+let args1 = args.join(" ").slice(1)
 if args = "daily"{
   let dailyembed = new Discord.RichEmbed()
   .setTitle("Model Daily Battle")
