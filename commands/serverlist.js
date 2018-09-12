1093135430c1d836dbbb7a153b1a4bc8206efd6d
qLau <38216868+qLau@users.noreply.github.com>
2018-09-12 15:54:58 +0300

    Create serverlist.js

---
+++ commands/serverlist.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let bicon = bot.user.displayAvatarURL;
+    let string = '';
+    bot.guilds.forEach(guild => {
+    string += guild.name + '\n';})
+    let bt = bot.user.username;
+    let botembed = new Discord.RichEmbed()
+        .setColor("#f71d54")
+        .addField("Lista Servere", string)
+        .setTimestamp()
+        .setFooter("Cerut de: " + message.author.username, message.author.avatarURL);
+    message.channel.send(botembed);
+}
+
+module.exports.help = {
+    name: "servers"
+}
