1c958fa41bff8030ab14ac9fefca593fc70af175
qLau <38216868+qLau@users.noreply.github.com>
2018-09-12 20:13:56 +0300

    Update sugestie.js

--- commands/sugestie.js
+++ commands/sugestie.js
@@ -11,7 +11,7 @@ try {
     const bug = args.join(" ")
     if (!bug) return message.channel.send('Te rog să introduci o sugestie!')
     const content = clean(`**${message.author.username}**#${message.author.discriminator} (${message.author.id}) a propus o sugestie:\n**Sugestie:**${bug}\nServer: **${message.guild.name}**\nID: **${message.guild.id}**`);
-    const id = '489480480605863946';
+    const id = '489483749604458497';
     new Promise((resolve, reject) => {
       superagent.post(`https://discordapp.com/api/channels/${id}/messages`)
         .set('Authorization', `Bot ${client.token}`).send({ content })
