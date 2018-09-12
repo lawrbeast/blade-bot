6272482d284b7702618fa5ba4a99906e1685e864
qLau <38216868+qLau@users.noreply.github.com>
2018-09-12 16:01:43 +0300

    Update serverlist.js

--- commands/serverlist.js
+++ commands/serverlist.js
@@ -2,9 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
+    let gicon = message.guild.iconURL;
     let string = '';
     bot.guilds.forEach(guild => {
-    string += guild.name + '\n';})
+    string += gicon + guild.name + '\n';})
     let bt = bot.user.username;
     let botembed = new Discord.RichEmbed()
         .setColor("#f71d54")
