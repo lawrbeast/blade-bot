a9f8a3e6837da46617bf57e104ff9b3e5fd535b2
qLau <38216868+qLau@users.noreply.github.com>
2018-09-13 21:41:21 +0300

    Update invites.js

--- commands/invites.js
+++ commands/invites.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 
-module.exports.run = (bot, message, args) => {
-    let invites = message.guild.fetchInvites().catch(error => {
-        return message.channel.send('Nu ai permisiunea necesară pentru a vizualiza invitațiile!');
+module.exports.run = async (bot, message, args) => {
+    let invites = await message.guild.fetchInvites().catch(error => {
+        return message.channel.send('Sorry, I don\'t have the proper permissions to view invites!');
     });
 
     invites = invites.array();
