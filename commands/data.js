1254bc7c193413a1248d33d170f08466f555dde0
qLau <38216868+qLau@users.noreply.github.com>
2018-09-13 21:55:50 +0300

    Update data.js

--- commands/data.js
+++ commands/data.js
@@ -6,6 +6,9 @@ var today = new Date()
 let Day = today.toString().split(" ")[0].concat("day");
 let Month = today.toString().split(" ")[1]
 let Year = today.toString().split(" ")[3]
+if(Day === "Thu"){
+    let Day = "Joi"
+};
 message.channel.send(`\`${Day}\` \`${Month}\` \`${Year}\`\n\`Ora zilei:\` \`${today.toString().split(" ")[4]}\``)
 }
 module.exports.help = {
