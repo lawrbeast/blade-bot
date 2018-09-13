2c592d62f74ed9f1ffd3d683720203d5249f51ae
qLau <38216868+qLau@users.noreply.github.com>
2018-09-13 21:53:45 +0300

    Update data.js

--- commands/data.js
+++ commands/data.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
 
 var today = new Date()
-let Day = today.toString().split(" ")[0].concat("ziua");
-let Month = today.toString().split(" ")[1].concat("luna");
-let Year = today.toString().split(" ")[3].concat("an")
+let Day = today.toString().split(" ")[0].concat("day");
+let Month = today.toString().split(" ")[1]
+let Year = today.toString().split(" ")[3]
 message.channel.send(`\`${Day}\` \`${Month}\` \`${Year}\`\n\`Ora zilei:\` \`${today.toString().split(" ")[4]}\``)
 }
 module.exports.help = {
