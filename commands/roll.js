614410e4e05166ba25bdf89e7fd65032804f2b5d
qLau <38216868+qLau@users.noreply.github.com>
2018-09-15 00:01:52 +0300

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 
 
     await message.channel.send("Arunc zarul...:game_die: :game_die: ")
-        .then(await(1500) => message.edit(`:game_die: Zarul a cazut pe fata cu numarul: ${rand()}`));
+        .then(await => message.edit(`:game_die: Zarul a cazut pe fata cu numarul: ${rand()}`));
 
 
 
