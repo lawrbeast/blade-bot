d988b26b05b7d479fbc0170af2dc614e77c3c684
qLau <38216868+qLau@users.noreply.github.com>
2018-09-14 23:48:50 +0300

    Update sfat.js

--- commands/sfat.js
+++ commands/sfat.js
@@ -2,7 +2,6 @@ const request = require('request');
 
 exports.run = (message, bot, send) => {
     let cn = request("http://api.adviceslip.com/advice", function(err, res, body) {
-        try {
             let cnj = JSON.parse(body)
             message.channel.send(cnj.slip.advice)
     });
