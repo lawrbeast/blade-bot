067b5703fcaa789ae37b214db8b1cde1be6021af
qLau <38216868+qLau@users.noreply.github.com>
2018-09-15 00:26:20 +0300

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -9,9 +9,9 @@ if(!args[0]){
 }
 if(args[0] === "duel"){
     let userduel = args[1];
+    if(!userduel) return message.channel.send("**Exemplu: d!roll duel @user");
     await message.channel.send("Arunc zarurile...:game_die: :game_die: ").then(msg => {msg.delete(2500)});
     await message.channel.send(`:game_die: Zarurile au cazut pe fetele cu numarul:\nJucator 1: ${message.author}: ${rand()}\nJucator 2: ${userduel}: ${rand1()}`);
- if(!userduel) return message.channel.send("**Exemplu: d!roll duel @user");
  }
 }
 
