29547c1095f56e69a606c909d0b344cdf9bf744c
qLau <38216868+qLau@users.noreply.github.com>
2018-09-14 23:43:19 +0300

    Create sfat.js

---
+++ commands/sfat.js
@@ -0,0 +1,15 @@
+const request = require('request');
+
+exports.run = (message, bot, send) => {
+    let cn = request("http://api.adviceslip.com/advice", function(err, res, body) {
+        try {
+            let cnj = JSON.parse(body)
+            message.channel.send(cnj.slip.advice)
+        } catch (e) {
+            return send("**Masinaria de sfaturi s-a stricat! :sad:**")
+        }
+    });
+}
+exports.help = {
+  name: "sfat"
+}
