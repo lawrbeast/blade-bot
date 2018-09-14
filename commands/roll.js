a74874ca2a8b91cd9621bd836b8be0910a22764c
qLau <38216868+qLau@users.noreply.github.com>
2018-09-15 00:14:23 +0300

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
 
     await message.channel.send("Arunc zarul...:game_die: :game_die: ").then(msg => {msg.delete(2500)});
     await message.channel.send(`:game_die: Zarul a cazut pe fata cu numarul: ${rand()}`);
-if(args[0 === "duel"){
+if(args[0] === "duel"){
     await message.channel.send("Arunc zarurile...:game_die: :game_die: ").then(msg => {msg.delete(2500)});
     await message.channel.send(`:game_die: Zarurile au cazut pe fetele cu numarul: ${rand()} - ${rand1()}`);
  }
