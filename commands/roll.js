03ebb9a807c760f4f4d3481a554b70e0e3052829
qLau <38216868+qLau@users.noreply.github.com>
2018-09-15 00:05:05 +0300

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -4,8 +4,8 @@ const rand = randomnum(1, 6);
 module.exports.run = async (bot, message, args) => {
 
 
-    await message.channel.send("Arunc zarul...:game_die: :game_die: ")
-        .then(message => await message.edit(`:game_die: Zarul a cazut pe fata cu numarul: ${rand()}`));
+    await message.channel.send("Arunc zarul...:game_die: :game_die: ").msg.delete(2500)
+        .then(message => message.channel.send(`:game_die: Zarul a cazut pe fata cu numarul: ${rand()}`));
 
 
 
