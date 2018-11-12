429145e6d60591fe4edb87848293e0fedfc8e657
qLau <38216868+qLau@users.noreply.github.com>
2018-11-12 16:49:08 +0200

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 
 exports.run = (bot, message, args) => {
     bot.unbanAuth = message.author;
-    const user = args[0];
+    const user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     const modlog = bot.channels.find('name', 'logs');
     if (!modlog) return message.reply('Nu gasesc channelul **logs**!');
     if (!user) return message.reply('Exemplu: d!unban ID & USER#1234.').catch(console.error);
