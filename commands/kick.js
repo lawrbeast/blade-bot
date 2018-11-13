63482941b134a4ba75538360a082a6b620cd25c6
qLau <38216868+qLau@users.noreply.github.com>
2018-11-13 19:48:20 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run =  (bot, message, args) => {
         let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(":no_entry_sign: Nu aveți permisiunea pentru a face asta!");
+        if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Nu ai permisiunile necesare.");
         if(!kUser) return message.channel.send("Nu ai mentionat un membru.").then(msg => {msg.delete(5000)});
         let kReason = args.join(" ").slice(22);
         if(!kReason) return message.channel.send("Nu ai mentionat un motiv.").then(msg => {msg.delete(5000)})
