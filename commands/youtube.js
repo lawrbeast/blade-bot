fa29ddd10a853dcc23d66357f1f0de32714676e5
qLau <38216868+qLau@users.noreply.github.com>
2018-11-13 19:08:33 +0200

    Update youtube.js

--- commands/youtube.js
+++ commands/youtube.js
@@ -10,12 +10,12 @@ module.exports.run = async (client, message, args) => {
       .then(results => {
 
         const ytEmbed = new Discord.RichEmbed()
-          .setAuthor(`YouTube search result for ${args}`.split(',').join(' '))
+          .setAuthor(`YouTube rezultate gasite pentru ${args}`.split(',').join(' '))
           .setThumbnail(results[0].thumbnails.high.url)
-          .setColor('#ffc1cc') //I personally use bubblegum pink!
-          .addField('Title', results[0].title, true)
+          .setColor('#cc0000') //I personally use bubblegum pink!
+          .addField('Titlu', results[0].title, true)
           .addField('Channel', results[0].channel.title, true)
-          .addField('Description', results[0].description)
+          .addField('Descriere', results[0].description)
           .addField('Link', `https://youtu.be/${results[0].id}`);
 
           message.channel.send({embed:ytEmbed});
