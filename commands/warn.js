9d65e9e82fbbb017ae5eb6e18909282cc0ef029f
qLau <38216868+qLau@users.noreply.github.com>
2018-11-13 19:36:42 +0200

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -8,18 +8,11 @@ module.exports.run = async (bot, message, args) => {
         .setColor("#0a9678")
         .addField("Te rog sa folosesti 竊ｴ", "!warn @User Motiv")
         .setFooter("~Comanda Warn~");
-
-        let cfu = new Discord.RichEmbed()
-        .setTitle ("Eroare!")
-        .setColor("#0a9678")
-        .addField("User negasit", "Nu gasesc acel user!");
-        if(!wUser) return message.channel.send({embed:cfu}).then(msg => {msg.delete(5000)});
+        if(!wUser) return message.channel.send("Nu ai menționat niciun un membru").then(msg => {msg.delete(5000)});
         let wReason = args.join(" ").slice(22)
-        if(!wReason){
-                let wReason = "fara motiv"
-        }
+        if(!wReason) wReason = "fara motiv"
         if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nu ai permisiunile necesare.");
-        if(wUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nu poti face asta unui administrator.");
+        if(wUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nu poți face asta unui administrator.");
 
         message.channel.send(`**${wUser} a fost avertizat!**`);
         wUser.send(`Ai primit warn pe **${message.guild.name}** de la **${message.author.tag}** cu motivul ${wReason}`)
