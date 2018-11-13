6c4fda3b569faaed9b7abcdbdc3a50b27e1b220d
qLau <38216868+qLau@users.noreply.github.com>
2018-11-13 19:59:59 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
   let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Nu ai permisiunile necesare.");
+  if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send(":no_entry_sign: Nu poti face asta unui administrator!");
   if(!bUser) return message.channel.send("Nu ai mentionat un membru\n**Exemplu: w!ban @membru.**").then(msg => {msg.delete(5000)});
   let bReason = args.join(" ").slice(22);
-  if(!bReason) return message.channel.send("**Te rog sa mentionezi un motiv\nExemplu: w!ban @user Motiv.**");
-  if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send(":no_entry_sign: Nu poti face asta unui administrator!");
+  if(!bReason) bReason = "fara motiv"
 
   let banEmbed = new Discord.RichEmbed()
   .setDescription(`Ban Info | Case 筐｢ ${bUser}`)
