6ff4c6c74b487f78f6ffe0515b72e9782919c444
qLau <38216868+qLau@users.noreply.github.com>
2018-11-13 19:56:43 +0200

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
         if(!wUser) return message.channel.send("Nu ai menționat un membru\n**Exemplu: o!warn @user**").then(msg => {msg.delete(5000)});
         let wReason = args.join(" ").slice(22)
         if(!wReason) wReason = "fara motiv"
-        if(wUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nu poți face asta unui administrator.");
+        if(wUser.hasPermission("MANAGE_MESSAGES")) message.channel.send(":no_entry_sign: Nu poti face asta unui administrator!");
 
         message.channel.send(`**${wUser} a fost avertizat!**`);
         wUser.send(`Ai primit warn pe **${message.guild.name}** de la **${message.author.tag}** cu motivul ${wReason}`)
