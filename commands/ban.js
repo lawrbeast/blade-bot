c8e9d5ae6c8a335db55a0ca4a2052345270e26ab
qLau <38216868+qLau@users.noreply.github.com>
2018-12-30 00:01:28 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,9 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = (bot, message, args) => {
   let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let bEmbed = new Discord.RichEmbed()
+  .setTitle('Comanda: o!ban')
+  .setDescription('**Descriere:** Banează un membru\n**Folosire:** o!ban @membru motiv\n**Exemplu:** o!ban @qLau motiv opțional')
+  if(!bUser) return message.channel.send({embed:bEmbed})
   if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Nu ai permisiunile necesare.");
   if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send(":no_entry_sign: Nu poti face asta unui administrator!");
-  if(!bUser) return message.channel.send("Nu ai mentionat un membru\n**Exemplu: w!ban @membru.**").then(msg => {msg.delete(5000)});
   let bReason = args.join(" ").slice(22);
   if(!bReason) bReason = "fara motiv"
 
