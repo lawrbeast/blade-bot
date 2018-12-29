b24d50ae6745616e67dbca309e44ae23bc8639f7
qLau <38216868+qLau@users.noreply.github.com>
2018-12-30 00:07:48 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,9 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run =  (bot, message, args) => {
         let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        let kEmbed = new Discord.RichEmbed()
+        .setTitle('Comanda: o!kick')
+        .setDescription('**Descriere:** Alungă un membru de pe server\n**Folosire:** o!kick @membru motiv\n**Exemplu:** o!kick @qLau motiv opțional')
+        if(!kUser) return message.channel.send({embed:kEmbed})
         if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Nu ai permisiunile necesare.");
-        if(!kUser) return message.channel.send("Nu ai mentionat un membru\n**Exemplu: o!kick @membru**").then(msg => {msg.delete(5000)});
-        if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry_sign: Nu poti face asta unui administrator!");
+        if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry_sign: Nu poți face asta unui administrator!");
         let kReason = args.join(" ").slice(22);
         if(!kReason) kReason = "fara motiv"
 
@@ -16,7 +19,7 @@ module.exports.run =  (bot, message, args) => {
         .addField("Membru", kUser, true)
         .addField("Moderator", message.author, true)
         .addField("Motiv", kReason, true);
-       
+
 
         let kickChannel = message.guild.channels.find(`name`, "logs");
         if(!kickChannel) return message.channel.send("Nu găsesc channel-ul `logs`.");
